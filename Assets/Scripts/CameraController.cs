ec3dff953e8606fe41d2c50a4e5b7385247fd716
Matt Swieboda <mswiebod@gmail.com>
2017-11-27 18:26:26 -0600

    Rotate and aim cue ball cleaned up with smoothing camera script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,19 +5,69 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 	public GameObject angledViewCamera;
 	public GameObject cueBall;
+	public GameObject cueGuide;
+	public GameObject cueGhostGuide;
 
 	private Camera cueCamera;
 	private Vector3 offset;
+	private float offsetYRotation;
 
 	// Use this for initialization
 	void Start () {
 		cueCamera = GetComponent<Camera>();
 		offset = transform.position - cueBall.transform.position;
+		offsetYRotation = cueGuide.transform.rotation.y - transform.rotation.y;
+	}
+
+	void LateUpdate () {
+
+		if (Input.GetButton("Alt Camera")) {
+			angledViewCamera.transform.LookAt(cueBall.transform);
+		}
+
+		smoothFollow(cueGhostGuide.transform);
+	}
+
+	private void smoothFollow(Transform target) {
+		float distance = 10.0f;
+		float height = 5.0f;
+		float heightDamping = 2.0f;
+		float rotationDamping = 3.0f;
+
+		// Early out if we don't have a target
+		if (!target) {
+			return;
+		}
+
+		// Calculate the current rotation angles
+		float wantedRotationAngle = target.eulerAngles.y;
+		float wantedHeight = target.position.y + height;
+
+		float currentRotationAngle = transform.eulerAngles.y;
+		float currentHeight = transform.position.y;
+
+		// Damp the rotation around the y-axis
+		currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+
+		// Damp the height
+		currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+		// Convert the angle into a rotation
+		var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+		// Set the position of the camera on the x-z plane to:
+		// distance meters behind the target
+		transform.position = target.position;
+		transform.position -= currentRotation * Vector3.forward * distance;
+
+		// Set the height of the camera
+		transform.position = new Vector3(transform.position.x,currentHeight,transform.position.z);
+
+		// Always look at the target
+		transform.LookAt(target);
 	}
 
 	void FixedUpdate () {
-		transform.position = cueBall.transform.position + offset;
-
 		if (Input.GetButton("Alt Camera")) {
 			cueCamera.enabled = false;
 			angledViewCamera.SetActive(true);
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Billiards Game made in Unity
 
 # keys
 
-W - hold to hit cue ball
-A/D - rotate around cue ball
+W - hold to build up speed, release to hit cue ball
+A/D - hold rotate cue ball aiming
 Tab - hold to switch to angled view
 R - jiggle rack (WIP to auto rack)
